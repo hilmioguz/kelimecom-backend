a9b7afd8a3f6ce4506ea919dddd35c85285a4c7f
DrCodeMonster <tarikeser@gmail.com>
2021-11-16 22:39:16 +0300

    s

--- src/core/services/helpers.vue
+++ src/core/services/helpers.vue
@@ -47,7 +47,7 @@ export default {
       dictionaries: [],
       packetsAll: [],
       uploadOptions: {
-        url: 'https://kelime-apiend:5001/v1/fileupload',
+        url: 'http://localhost:5001/v1/fileupload',
         acceptedFiles: 'image/*',
         thumbnailWidth: 150,
         addRemoveLinks: true,
